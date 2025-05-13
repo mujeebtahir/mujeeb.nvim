1e321a5a7680f77c973330a6e94e17f4f32405e3
Mujeeb Tahir <mujeebvhr@gmail.com>
2025-05-14 04:30:07 +0500

    feat(custom.plugins.nvim): add plugins bufferline, Ansi-Esc, vim-hightlighter, no-neck-pain, Comment, todo-comments

--- init.lua
+++ init.lua
@@ -983,6 +983,7 @@ require('lazy').setup({
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
+  require 'custom.plugins.init',
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   -- { import = 'custom.plugins' },
   --
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,59 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- TODO: Plugins to try
+  -- * timewarrior: https://www.reddit.com/r/neovim/comments/1awiy1t/neovim_timetracking_system_using_timewarrior/
+  -- * obsidian: https://www.reddit.com/r/neovim/comments/1awiy1t/neovim_timetracking_system_using_timewarrior/
+  -- * Journaling?
+  -- * Org-mode from emacs?
+  -- * Neovide (sick animations, a gui for neovim) https://neovide.dev/
+  -- * barbar.vim for buffer tabs
+  --
+  -- TODO: Fix things
+  -- * Fix opts related to tab and spaces. Tab should be 8 not 4.
+  --
+  -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  -- {
+  --   'mg979/vim-visual-multi',
+  --   branch = 'master',
+  --   init = function()
+  --     vim.g.VM_maps = {
+  --       ['Find Under'] = '<C-n>',
+  --     }
+  --   end,
+  -- },
+  'powerman/vim-plugin-AnsiEsc', -- he AnsiEsc.vim file, when sourced, will conceal Ansi escape sequences but will cause subsequent text to be colored as the escape sequence specifies.
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  {
+    'shortcuts/no-neck-pain.nvim',
+    version = '*',
+    opts = {
+      width = 127,
+    },
+  },
+  {
+    'azabiong/vim-highlighter',
+    init = function()
+      -- settings
+    end,
+  },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    event = 'VeryLazy', -- or use "BufReadPre" if you want it earlier
+    config = function()
+      require('bufferline').setup {}
+    end,
+  },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+}
