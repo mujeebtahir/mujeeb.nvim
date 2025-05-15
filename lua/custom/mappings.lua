bb5423a86d77cc09235cb6e29ed3b8d1d6f10eef
Mujeeb Tahir <mujeebvhr@gmail.com>
2025-05-16 02:02:44 +0500

    feat(custom.mappings): diff commands, diagnostic commands

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -32,8 +32,54 @@ vim.keymap.set('n', '<leader>t', ':terminal<CR>', { desc = 'Terminal Opens a new
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- Disable the AnsiEsc plugin's keymap, it overlaps with <leader>sw "Treesitter word search"
 vim.keymap.del('n', '<leader>swp')
+
+-- Diff Commands/Mappings
+vim.keymap.set('n', '<leader><leader>v', function()
+  if next(require('diffview.lib').views) == nil then
+    vim.cmd 'DiffviewOpen'
+  else
+    vim.cmd 'DiffviewClose'
+  end
+end)
+vim.api.nvim_create_user_command('DiffWith', function()
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local cwd = vim.fn.expand '%:p:h'
+
+  require('telescope').extensions.file_browser.file_browser {
+    path = cwd,
+    cwd = cwd,
+    prompt_path = true,
+    select_buffer = true,
+    attach_mappings = function(_, map)
+      map('i', '<CR>', function(prompt_bufnr)
+        local action_state = require 'telescope.actions.state'
+        local actions = require 'telescope.actions'
+        local selected = action_state.get_selected_entry().value
+        actions.close(prompt_bufnr)
+        vim.cmd('vert diffsplit ' .. vim.fn.fnameescape(selected))
+      end)
+      return true
+    end,
+  }
+end, {})
+vim.keymap.set('n', '<leader>fd', ':DiffWith<CR>', { desc = '[F]ile [D]iff with current file' })
+
 vim.keymap.set('n', '<leader>yf', ':let @+ = expand("%:t")<CR>', { desc = 'Copy Just The FileName to Register', silent = true, noremap = true })
 vim.keymap.set('n', '<leader>yp', ':let @+ = expand("%:p")<CR>', { desc = 'Copy Just The Absolute File Path to Register', silent = true, noremap = true })
 vim.keymap.set('n', '<leader>yr', ':let @+ = expand("%")<CR>', { desc = 'Copy Relative File Path to Register', silent = true, noremap = true })
